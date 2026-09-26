3ca257b58b17d015a7380c0a969dbda6f1a96986
Mr. Denman Mills <alexmillsmail@gmail.com>
2026-09-25 20:27:57 -0400

    config: export BeamScale provider config

--- src/rust/lib.rs
+++ src/rust/lib.rs
@@ -100,7 +100,8 @@ pub use beamscale::{
 };
 #[cfg(feature = "config")]
 pub use config::{
-    CloudflareDurableObjectProviderConfig, EnvBinding, EnvKind, FiduciaProviderConfig,
+    BeamScaleCriticalSectionProviderConfig, CloudflareDurableObjectProviderConfig, EnvBinding,
+    EnvKind, FiduciaProviderConfig,
     LOCK_CONFIG_SCHEMA_V1, LocalFileProviderConfig, LockConfigError, LockProfileConfig,
     MAX_CONFIG_ENVS, MAX_CONFIG_PROFILES, MAX_RENEW_INTERVAL_MS, MAX_RETRY_INTERVAL_MS, MAX_TTL_MS,
     MAX_WAIT_TIMEOUT_MS, OresLockConfigV1, OuterLeaseAuthority, PostgresLockScope,
